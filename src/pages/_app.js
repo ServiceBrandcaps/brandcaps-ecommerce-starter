a72c405a24c349b63e4d079877ef19331969ff3a
Matias Monti <matigaris@gmail.com>
2025-09-18 22:03:09 -0300

    Se agrego el favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,19 @@ import "../styles/globals.css";
 import { AuthProvider } from "../context/AuthContext";
 import { CartProvider } from "../context/CartContext";
 import { ToastProvider } from "@/components/Toast";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
       <CartProvider>
         <ToastProvider>
+          <Head>
+            <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+            <link rel="icon" href="/favicon.ico" sizes="any" />
+            <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+            <meta name="theme-color" content="#000000" />
+          </Head>
           <Component {...pageProps} />
         </ToastProvider>
       </CartProvider>
